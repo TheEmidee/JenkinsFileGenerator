d76b142149dea3288fbe55e71665bb396cacbeb6
Michael Delva <michael@emidee.net>
2025-07-29 12:04:12 +0200

    Removed option abort_running_builds from the Utils as this can now be done through the pipeline properties

--- generator/features/utils.py
+++ generator/features/utils.py
@@ -1,18 +1,13 @@
 """Utilities feature for the Jenkins File Generator."""
 
-from typing import Any, Dict, Optional
-from pydantic import BaseModel, Field
+from typing import Any, Dict
+from pydantic import BaseModel
 
 from generator.core.base_feature import BaseFeature, FeatureConfig
 
 
 class UtilsConfig(FeatureConfig):
     """Configuration model for the utils feature."""
-
-    abort_running_builds: Optional[bool] = Field(
-        default=True,
-        description="If true, aborts any running builds before starting a new one.",
-    )
 
 
 class UtilsFeature(BaseFeature):
